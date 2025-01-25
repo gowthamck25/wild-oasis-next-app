1792429b10038afccf76aafe24b76d33759154c5
gowthamtmrw <gowtham2@tmrw.in>
2025-01-25 23:45:03 +0530

    first commit

---
+++ app/cabins/page.js
@@ -0,0 +1,40 @@
+import CabinList from "@/app/_components/CabinList";
+import Spinner from "@/app/_components/Spinner";
+import { Suspense } from "react";
+import Filter from "../_components/Filter";
+
+export const revalidate = 3600;
+// export const revalidate = 15;
+
+export const metadata = {
+  title: "Cabins",
+};
+
+
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
+  return (
+    <div>
+      <h1 className="mb-5 text-4xl font-medium text-accent-400">
+        Ou r Luxury Cabins
+      </h1>
+      <p className="mb-10 text-lg text-primary-200">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
+      </Suspense>
+    </div>
+  );
+}
